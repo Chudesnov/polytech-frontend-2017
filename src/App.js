89ff6f19c492f1fde7f1797dfc1e1afcfef003f5
justplesh <antonkuklin99@yandex.ru>
2017-09-29 00:20:19 +0300

    ISSUE-1 Добавил в 'App.js' компонент 'Dream', а так же добавил его в сам 'App' компонент

--- src/App.js
+++ src/App.js
@@ -13,9 +13,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <Dream />
       </div>
     );
   }
+}
+
+class Dream extends Component {
+    render() {
+        return (
+          <div className="Dream">
+              <p>Biba</p>
+          </div>
+        );
+    }
 }
 
 export default App;
