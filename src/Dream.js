170816134bbc086a1c062ca42c9dfa861a86a43c
justplesh <antonkuklin99@yandex.ru>
2017-09-29 00:53:59 +0300

    ISSUE-1 В компоненте 'Dream' вместо надписи сделал инпут

--- src/Dream.js
+++ src/Dream.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class Dream extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
     render() {
         return (
             <div className="Dream">
-                <p>Biba</p>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
             </div>
         );
     }
