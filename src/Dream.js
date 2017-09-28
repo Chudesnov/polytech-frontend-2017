5de9eda3ee54597e0627733ddd4c01cc6b17e810
justplesh <antonkuklin99@yandex.ru>
2017-09-29 01:05:48 +0300

    ISSUE-1 Добавил кнопку, повесил на нее обработчик клика, вывожу после него строку с введенным текстом в инпут, добавил стиль для класса дрим

--- src/Dream.js
+++ src/Dream.js
@@ -4,19 +4,30 @@ class Dream extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            fieldValue: '',
+            dreamValue: ''
+        };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({fieldValue: event.target.value});
+    }
+
+    handleClick() {
+        this.setState({dreamValue: this.state.fieldValue});
     }
 
     render() {
         return (
             <div className="Dream">
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                <input type="text" value={this.state.fieldValue} onChange={this.handleChange} />
+                <button onClick={this.handleClick}>Make it real</button>
+                <br />
+                <span>{this.state.dreamValue}</span>
             </div>
         );
     }
